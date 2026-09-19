8655a5e65a5e31db6a0cc2780c2e87f7a291165f
Chloe Ireneo <kkzuhra.bloom@gmail.com>
2026-09-19 09:33:12 +0800

    Update CircularGarden.py

--- CircularGarden.py
+++ CircularGarden.py
@@ -14,8 +14,8 @@ floor_area = math.floor(area)
 ceiling_area = math.ceil(area)
 
 # OUTPUT STAGE
-print(f"Area of the garden: {area:.2f}\n"
-      f"Circumference of the garden: {circumference:.2f}\n"
+print(f"Area of the garden: {area:.2f} square meters \n"
+      f"Circumference of the garden: {circumference:.2f} meters\n"
       f"Square root of the area: {root_area:.2f}\n"
-      f"Area rounded down: {floor_area:.2f}\n"
-      f"Area rounded up: {ceiling_area:.2f}") # :.2f only displays 2 decimal places
+      f"Area rounded down: {floor_area:.2f} square meters\n"
+      f"Area rounded up: {ceiling_area:.2f} square meters") # :.2f only displays 2 decimal places
